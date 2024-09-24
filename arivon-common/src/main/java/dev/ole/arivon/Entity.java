f63970874d14a184b6513f4dbe8448c680a4eb45
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-24 22:03:30 +0200

    Add Entity annotation to common module
    This commit introduces a new `Entity` annotation in the `arivon-common` module. The annotation is designed to be used at the type level and includes an option to ignore superclass properties. This will facilitate marking classes as database entities and customizing their behavior.

---
+++ arivon-common/src/main/java/dev/ole/arivon/Entity.java
@@ -0,0 +1,13 @@
+package dev.ole.arivon;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Entity {
+
+    boolean ignoreSuperClass() default false;
+}
