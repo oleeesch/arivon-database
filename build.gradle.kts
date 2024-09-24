e5156c205867e68efd2689a7f676af9601fd10cb
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-24 21:46:10 +0200

    Add gradle version catalog and configure project publishing
    Introduced gradle/libs.versions.toml to manage dependency versions centrally for the project. Updated build.gradle.kts to apply necessary plugins, set up dependencies, and configure Maven publishing settings, including setting up project metadata for the publication.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,57 @@
 plugins {
-    id("java")
 }
 
-group = "dev.ole.arivon"
-version = "1.0-SNAPSHOT"
+allprojects {
+    apply(plugin = "java-library")
+    apply(plugin = "maven-publish")
 
-repositories {
-    mavenCentral()
-}
+    version = "1.0-SNAPSHOT"
+    group = "dev.ole.arivon"
 
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
+    repositories {
+        mavenCentral()
+        maven(url = "https://s01.oss.sonatype.org/content/repositories/snapshots/")
+    }
 
-tasks.test {
-    useJUnitPlatform()
+    dependencies {
+        "annotationProcessor"(rootProject.libs.lombok)
+        "implementation"(rootProject.libs.bundles.utils)
+    }
+
+    tasks.withType<JavaCompile>().configureEach {
+        sourceCompatibility = JavaVersion.VERSION_21.toString()
+        targetCompatibility = JavaVersion.VERSION_21.toString()
+        options.encoding = "UTF-8"
+    }
+
+    extensions.configure<PublishingExtension> {
+        publications {
+            create("library", MavenPublication::class.java) {
+                from(project.components.getByName("java"))
+
+                pom {
+                    name.set(project.name)
+                    url.set("https://github.com/oleeesch/arivon-database")
+                    description.set("Reflection/Data libary")
+                    licenses {
+                        license {
+                            name.set("Apache License")
+                            url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                        }
+                    }
+                    developers {
+                        developer {
+                            name.set("Ole Schumacher")
+                            email.set("oleschumacher1801@gmail.com")
+                        }
+                    }
+
+                    scm {
+                        url.set("https://github.com/oleeesch/arivon-database")
+                        connection.set("https://github.com/oleeesch/arivon-database.git")
+                    }
+                }
+            }
+        }
+    }
 }
