057331711c4fbd22721a4ad3a5fd779c57a437ca
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-24 22:03:35 +0200

    Add abstract Filter class for filtering logic
    Introduced a new abstract class `Filter` in `arivon-common` module. This class defines a generic structure for filtering with a required `filter` method and an optional `requirementCheck` method. This setup provides a flexible foundation for extending specific filter implementations.

---
+++ arivon-common/src/main/java/dev/ole/arivon/filtering/Filter.java
@@ -0,0 +1,20 @@
+package dev.ole.arivon.filtering;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+
+@AllArgsConstructor
+@Accessors(fluent = true)
+@Getter
+public abstract class Filter<T, R> {
+
+    private String id;
+    private R value;
+
+    public abstract T filter();
+
+    public boolean requirementCheck(Class<?> clazz) {
+        return true;
+    }
+}
