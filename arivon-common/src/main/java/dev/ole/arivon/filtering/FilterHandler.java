70bb050a726608b626dd4f3957653e30a93b1e3d
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-25 16:21:50 +0200

    Add new filtering methods to FilterHandler
    Introduced 'like', 'matchIgnoreCase', 'between', 'min', and 'max' methods to enhance filtering capabilities. Updated Javadoc comments for better clarity on 'noneMatch' method parameters. These additions improve the flexibility and functionality of filter creation.

--- arivon-common/src/main/java/dev/ole/arivon/filtering/FilterHandler.java
+++ arivon-common/src/main/java/dev/ole/arivon/filtering/FilterHandler.java
@@ -5,19 +5,43 @@ public interface FilterHandler<T, R> {
     /**
      * Creates a filter to include values that match the specified value.
      *
-     * @param id    The identifier for the filter
+     * @param id    The identifier for the filter.
      * @param value The value to match.
      * @return A filter object.
      */
     Filter<T, R> match(String id, Object value);
 
     /**
-     * Creates a filter to exclude values that match any of the specified value.
+     * Creates a filter to exclude values that match any of the specified values.
      *
-     * @param id    The identifier for the filter
-     * @param value The value to exclude.
+     * @param id     The identifier for the filter.
+     * @param value The values to exclude.
      * @return A filter object.
      */
     Filter<T, R> noneMatch(String id, Object value);
+
+    /**
+     * Creates a filter to include values that contain the specified text.
+     *
+     * @param id    The identifier for the filter.
+     * @param value The text to match.
+     * @return A filter object.
+     */
+    Filter<T, R> like(String id, String value);
+
+    /**
+     * Creates a filter to include values that match the specified value and ignore upper or lower case.
+     *
+     * @param id    The identifier for the filter.
+     * @param value The value to match.
+     * @return A filter object.
+     */
+    Filter<T, R> matchIgnoreCase(String id, String value);
+
+    Filter<T, R> between(String id, Number min, Number max);
+
+    Filter<T, R> min(String id, Number min);
+
+    Filter<T, R> max(String id, Number max);
 
 }
