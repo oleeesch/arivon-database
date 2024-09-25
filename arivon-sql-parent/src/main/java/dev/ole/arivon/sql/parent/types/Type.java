d0380d0862ea78495fe97ca63c308b80e6bd559c
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-25 16:42:27 +0200

    Add Type class to handle repository type management
    This commit introduces a new Type class with attributes for type name and predicate for repository entries. The class includes methods for creating Type instances based on class compatibility or custom predicates. Lombok annotations are used for getter methods and fluent accessors.

---
+++ arivon-sql-parent/src/main/java/dev/ole/arivon/sql/parent/types/Type.java
@@ -0,0 +1,32 @@
+package dev.ole.arivon.sql.parent.types;
+
+import dev.ole.arivon.RepositoryEntry;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.function.Predicate;
+
+@Getter
+@Accessors(fluent = true)
+@AllArgsConstructor
+public class Type {
+
+    private String type;
+    private Predicate<RepositoryEntry> predicate;
+
+    public static @NotNull Type of(String type, Class<?>... compatibleClass) {
+        return new Type(type, it -> Arrays.asList(compatibleClass).contains(it.clazz()));
+    }
+
+    public static @NotNull Type of(String type, Predicate<RepositoryEntry> predicate) {
+        return new Type(type, predicate);
+    }
+
+    @Override
+    public String toString() {
+        return type;
+    }
+}
