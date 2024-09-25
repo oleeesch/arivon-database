057b1889638be00c00301d16aa914338949b34f6
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-25 15:52:19 +0200

    Add RepositoryMapEntry to handle map-specific entries
    Introduce a new `RepositoryMapEntry` class extending `RepositoryExternalEntry` for handling map entries efficiently. This class provides methods to handle key-value pair entries and read map values.

---
+++ arivon-common/src/main/java/dev/ole/arivon/external/RepositoryMapEntry.java
@@ -0,0 +1,44 @@
+package dev.ole.arivon.external;
+
+import dev.ole.arivon.RepositoryEntry;
+import dev.ole.arivon.RepositoryExternalEntry;
+import dev.ole.arivon.common.GenericReader;
+import dev.ole.arivon.common.Pair;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Unmodifiable;
+
+import java.lang.reflect.Field;
+import java.util.*;
+
+@Getter
+@Accessors(fluent = true)
+public final class RepositoryMapEntry extends RepositoryExternalEntry {
+
+    private final RepositoryEntry keyEntry;
+    private final RepositoryEntry valueEntry;
+
+    public RepositoryMapEntry(String id, @NotNull Field field, RepositoryExternalEntry parent) {
+        super(id, field.getType(), parent);
+
+        var genericData = GenericReader.types(field);
+        this.keyEntry = new RepositoryEntry(field.getName() + "_key", genericData[0], this);
+        this.valueEntry = new RepositoryEntry(field.getName() + "_value", genericData[1], this);
+    }
+
+    @Contract(pure = true)
+    @Override
+    public @Unmodifiable @NotNull List<RepositoryEntry> children() {
+        return List.of(keyEntry, valueEntry);
+    }
+
+    @Contract(pure = true)
+    @Override
+    public @NotNull Collection<Object> readValues(Object parent) {
+        var elements = new ArrayList<>();
+        ((Map<?, ?>) parent).forEach((o, o2) -> elements.add(new Pair<>(o, o2)));
+        return elements;
+    }
+}
