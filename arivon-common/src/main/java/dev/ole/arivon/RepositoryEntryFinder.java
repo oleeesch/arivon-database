d0a06d47f56b2feef8a49dfc187c45fea11d8d09
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-25 15:52:07 +0200

    Add RepositoryEntryFinder for entry resolution
    Introduce a utility class, `RepositoryEntryFinder`, to determine the type of repository entries based on the class and field provided. This includes handling of various types such as Maps, Collections, and Records, and throws an exception for unsupported types.

---
+++ arivon-common/src/main/java/dev/ole/arivon/RepositoryEntryFinder.java
@@ -0,0 +1,53 @@
+package dev.ole.arivon;
+
+import dev.ole.arivon.common.JavaUtils;
+import dev.ole.arivon.external.RepositoryCollectionEntry;
+import dev.ole.arivon.external.RepositoryMapEntry;
+import dev.ole.arivon.external.RepositoryObjectEntry;
+import dev.ole.arivon.transformers.RecordTransformer;
+import lombok.experimental.UtilityClass;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.Field;
+import java.util.Collection;
+import java.util.Map;
+import java.util.UUID;
+
+@UtilityClass
+public final class RepositoryEntryFinder {
+
+    @Contract("_, _, _, _ -> new")
+    public static @NotNull RepositoryEntry find(Class<?> clazz, Field field, String id, RepositoryExternalEntry parent) {
+        var externalId = parent != null ? parent.id() + "_" + id : id;
+
+        if (JavaUtils.JAVA_ELEMENTS.contains(clazz) || clazz.isEnum() || clazz.isPrimitive() || clazz.equals(UUID.class)) {
+            return new RepositoryEntry(id, clazz, parent);
+        }
+
+        if (Map.class.isAssignableFrom(clazz)) {
+            return new RepositoryMapEntry(externalId, field, parent);
+        }
+
+        if (Collection.class.isAssignableFrom(clazz)) {
+            return new RepositoryCollectionEntry(externalId, field, parent);
+        }
+
+        if (!clazz.isSynthetic()) {
+            var entry = new RepositoryObjectEntry(externalId, clazz, parent);
+
+            if (clazz.isRecord()) {
+                // we must set a reflection transformer for records
+                entry.children().forEach(it -> it.constants().put(RepositoryConstant.TRANSFORMER, RecordTransformer.RECORD_TRANSFORMER));
+            }
+            return entry;
+        }
+
+        throw new IllegalArgumentException("Unsupported entry type: " + clazz);
+    }
+
+    @Contract("_, _, _ -> new")
+    public static @NotNull RepositoryEntry find(Field field, String id, RepositoryExternalEntry parent) {
+        return find(field.getType(), field, id, parent);
+    }
+}
