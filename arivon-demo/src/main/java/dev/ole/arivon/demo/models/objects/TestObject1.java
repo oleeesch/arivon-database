70ff8355b124fd6e894c455671cb70d84a96dd02
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-25 17:52:05 +0200

    Add TestObject1 model with Lombok annotations
    Introduce TestObject1 class with `permission` and `age` fields. This class uses Lombok for generating constructor and getter methods, promoting cleaner and more maintainable code.

---
+++ arivon-demo/src/main/java/dev/ole/arivon/demo/models/objects/TestObject1.java
@@ -0,0 +1,13 @@
+package dev.ole.arivon.demo.models.objects;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TestObject1 {
+
+    private final String permission;
+    private final int age;
+
+}
