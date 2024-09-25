fad02534b6690a29488432c51dd984a17ab5d583
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-25 15:51:47 +0200

    Add ConstantPool class
    Introduce the ConstantPool class with methods to manage constants using a ConcurrentHashMap. This includes methods for adding, checking, and cloning constants, providing thread-safe access to these configurations.

---
+++ arivon-common/src/main/java/dev/ole/arivon/constant/ConstantPool.java
@@ -0,0 +1,45 @@
+package dev.ole.arivon.constant;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public final class ConstantPool {
+
+    public final Map<Constant<?>, Object> constants = new ConcurrentHashMap<>();
+
+    public void option(Constant<Void> constant) {
+        constants.put(constant, true);
+    }
+
+    public <T> T put(Constant<T> constant, T value) {
+        constants.put(constant, value);
+        return value;
+    }
+
+    public boolean has(Constant<?> constant) {
+        return constants.containsKey(constant);
+    }
+
+    public <T> T  constantOrCreate(Constant<T> constant, T defaultValue) {
+        if(has(constant)) {
+            return constant(constant);
+        }
+        this.constants.put(constant, defaultValue);
+        return defaultValue;
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T constant(Constant<T> constant) {
+        return (T) constants.get(constant);
+    }
+
+    public void cloneConstants(@NotNull ConstantPool pool) {
+        this.constants.putAll(pool.constants);
+    }
+
+    public void cloneConstants(@NotNull ConstantPool pool, Constant<?> constant) {
+        this.constants.put(constant, pool.constant(constant));
+    }
+}
