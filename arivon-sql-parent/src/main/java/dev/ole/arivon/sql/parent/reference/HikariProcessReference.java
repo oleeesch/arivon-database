9b44c9ef7d1e33e3af1ebdce739b24d7c56e1aba
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-25 16:41:34 +0200

    Add HikariProcessReference class for SQL query execution
    Introduced HikariProcessReference for managing HikariConnection-based SQL executions. This class allows direct query execution and appending deprecated methods for backward compatibility. It includes utility methods for handling SQL queries with customizable parameters and result processors.

---
+++ arivon-sql-parent/src/main/java/dev/ole/arivon/sql/parent/reference/HikariProcessReference.java
@@ -0,0 +1,44 @@
+package dev.ole.arivon.sql.parent.reference;
+
+import dev.ole.arivon.process.ProcessReference;
+import dev.ole.arivon.sql.parent.connection.HikariConnection;
+import dev.ole.arivon.sql.parent.connection.HikariConnectionFunction;
+import dev.ole.arivon.sql.parent.connection.HikariStatementBuilder;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+
+import java.sql.ResultSet;
+import java.util.*;
+
+@Getter
+@Accessors(fluent = true)
+public final class HikariProcessReference extends ProcessReference<HikariConnection> {
+
+    private final List<HikariReferenceData<ResultSet>> sqlQueries = new ArrayList<>();
+
+    public HikariProcessReference(HikariConnection connection) {
+        super(connection);
+    }
+
+    public <R> R directly(String query, Object[] arguments, HikariStatementBuilder<R> consumer) {
+        return this.connection().query(query, arguments, consumer);
+    }
+
+    @Deprecated
+    public HikariProcessReference append(String query, Object[] parameters, HikariConnectionFunction<ResultSet> consumer) {
+        this.sqlQueries.add(new HikariReferenceData<>(query, parameters, consumer));
+        return this;
+    }
+
+    @Deprecated
+    public HikariProcessReference append(String query, Object[] parameters) {
+        return this.append(query, parameters, resultSet -> {
+        });
+    }
+
+    @Deprecated
+    public HikariProcessReference append(String query) {
+        return this.append(query, new Object[0]);
+    }
+
+}
