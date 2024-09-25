3764926b3727b46c9777677c185e02279b924231
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-25 17:52:12 +0200

    Add TestObject2 model class
    Introduces a new immutable model class `TestObject2` with `name` and `expire` fields. Uses Lombok annotations for boilerplate code reduction.

---
+++ arivon-demo/src/main/java/dev/ole/arivon/demo/models/objects/TestObject2.java
@@ -0,0 +1,13 @@
+package dev.ole.arivon.demo.models.objects;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public final class TestObject2 {
+
+    private final String name;
+    private final long expire;
+
+}
