2495f86f53b60a59e6ce5380b5761e4130b3e77d
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-25 16:40:16 +0200

    Add TypeModel class to handle type transformations
    Introduce a new TypeModel class extending the Type class, allowing type transformations using a Function and a Predicate. This enhances the modularity and flexibility for manipulating repository entries.

---
+++ arivon-sql-parent/src/main/java/dev/ole/arivon/sql/parent/types/TypeModel.java
@@ -0,0 +1,28 @@
+package dev.ole.arivon.sql.parent.types;
+
+import dev.ole.arivon.RepositoryEntry;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public final class TypeModel extends Type {
+
+    private final Function<Class<?>, String> result;
+
+    public TypeModel(Function<Class<?>, String> result, Predicate<RepositoryEntry> predicate) {
+        super(null, predicate);
+        this.result = result;
+    }
+
+    @Contract("_, _ -> new")
+    public static @NotNull Type of(Function<Class<?>, String> result, Predicate<RepositoryEntry> predicate) {
+        return new TypeModel(result, predicate);
+    }
+
+    @Contract("_ -> new")
+    public @NotNull Type model(Class<?> clazz) {
+        return new Type(result.apply(clazz), predicate());
+    }
+}
