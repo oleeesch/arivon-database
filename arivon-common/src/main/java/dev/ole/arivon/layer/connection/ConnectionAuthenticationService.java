887a05f87bd29ce8942d8be30f7bf10d3dab1bf8
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-25 16:21:46 +0200

    Add ConnectionAuthenticationService.java
    Implement ConnectionAuthenticationService to handle authentication credentials. This includes creating a configuration file, reading, updating, and appending credential contexts as JSON objects. Additionally, it ensures that connection states are checked and managed.

---
+++ arivon-common/src/main/java/dev/ole/arivon/layer/connection/ConnectionAuthenticationService.java
@@ -0,0 +1,66 @@
+package dev.ole.arivon.layer.connection;
+
+import com.google.gson.*;
+import dev.ole.arivon.common.JsonUtils;
+import lombok.SneakyThrows;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Objects;
+import java.util.Set;
+
+public final class ConnectionAuthenticationService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConnectionAuthenticationService.class);
+
+    static {
+        createConfigurationFile();
+    }
+
+    @SneakyThrows
+    private static void createConfigurationFile() {
+        // Create if the not exists
+        Files.newByteChannel(ConnectionAuthenticationPath.CONFIGURATION_PATH, Set.of(StandardOpenOption.CREATE, StandardOpenOption.WRITE)).close();
+    }
+
+    public static void appendCredentials(ConnectableLayer<?, ?> connectableLayer, ConnectionAuthentication authentication) {
+        var elements = readCredentialsContext();
+        for (var credentials : elements) {
+            if (!credentials.isJsonObject()) {
+                continue;
+            }
+            var credentialsAsJsonObject = credentials.getAsJsonObject();
+
+            if (!credentialsAsJsonObject.get("id").getAsString().equalsIgnoreCase(connectableLayer.id())) {
+                continue;
+            }
+
+            if (!credentialsAsJsonObject.get("active").getAsBoolean()) {
+                LOGGER.warn("Repository use {}, but session is inactive.", connectableLayer.id());
+                return;
+            }
+
+            if(!connectableLayer.connection().isConnected()) {
+                connectableLayer.connection().connect(JsonUtils.GSON.fromJson(credentials, authentication.getClass()));
+            }
+            return;
+        }
+        elements.add(JsonUtils.GSON.toJsonTree(authentication));
+        updateCredentialsContext(elements);
+    }
+
+    @SneakyThrows
+    private static @NotNull JsonArray readCredentialsContext() {
+        var authenticationArray = JsonUtils.GSON.fromJson(Files.newBufferedReader(ConnectionAuthenticationPath.CONFIGURATION_PATH), JsonArray.class);
+        return Objects.requireNonNullElseGet(authenticationArray, JsonArray::new);
+    }
+
+    @SneakyThrows
+    private static void updateCredentialsContext(JsonArray elements) {
+        Files.writeString(ConnectionAuthenticationPath.CONFIGURATION_PATH, JsonUtils.GSON.toJson(elements));
+    }
+}
