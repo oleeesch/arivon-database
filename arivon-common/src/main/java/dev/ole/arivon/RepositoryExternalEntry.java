daff127649c4dc93689ceba6951be8984f82da8b
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-25 15:52:11 +0200

    Add `RepositoryExternalEntry` class
    Introduce the `RepositoryExternalEntry` class to provide an abstraction layer for repository entries with child elements. This class includes methods for managing children, filtering primary keys, and reading values.

---
+++ arivon-common/src/main/java/dev/ole/arivon/RepositoryExternalEntry.java
@@ -0,0 +1,35 @@
+package dev.ole.arivon;
+
+import lombok.Getter;
+import lombok.experimental.Accessors;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+@Getter
+@Accessors(fluent = true)
+public abstract class RepositoryExternalEntry extends RepositoryEntry {
+
+    private final List<RepositoryEntry> children = new LinkedList<>();
+
+    public RepositoryExternalEntry(String id, Class<?> clazz, RepositoryExternalEntry type) {
+        super(id, clazz, type);
+    }
+
+    public List<RepositoryEntry> primaries() {
+        return this.children.stream().filter(it -> it.constants().has(RepositoryConstant.PRIMARY_KEY)).toList();
+    }
+
+    public void children(RepositoryEntry entry) {
+        if (entry instanceof RepositoryExternalEntry) {
+            this.children.add(entry);
+        } else {
+            this.children.add(0, entry);
+        }
+    }
+
+    public Collection<Object> readValues(Object parent) {
+        return List.of(parent);
+    }
+}
