e4b175854df61aeec2ff6086847a89b4a4e6d61a
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-25 16:23:06 +0200

    Add QueryMethod interface to arivon-common module
    This new interface includes methods for common database operations such as create, update, delete, and various query functions. It will facilitate querying capabilities with methods like find, count, sum, and order, enhancing the modularity and functionality of the codebase.

---
+++ arivon-common/src/main/java/dev/ole/arivon/query/QueryMethod.java
@@ -0,0 +1,33 @@
+package dev.ole.arivon.query;
+
+import dev.ole.arivon.Ordering;
+
+import java.util.List;
+
+public interface QueryMethod<V> {
+
+    void create(Query<?> query, V value);
+
+    void update(Query<?> query, V value);
+
+    void delete(Query<?> query);
+
+    boolean exists(Query<?> query);
+
+    V findFirst(Query<?> query);
+
+    long count(Query<?> query);
+
+    List<V> find(Query<?> query);
+
+    long sum(Query<?> query, String id);
+
+    double average(Query<?> query, String id);
+
+    V min(String id);
+
+    V max(String id);
+
+    List<V> order(Query<?> query, String id, Ordering ordering);
+
+}
