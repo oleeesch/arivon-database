438ffd82af8f888bfe84331c8a1cef4259666d80
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-25 17:38:47 +0200

    Add new modules to settings.gradle.kts
    Included new modules for MySQL, MariaDB, H2, PostgreSQL, and Redis. This will enable support for additional databases and document storage in the project.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,9 @@
 rootProject.name = "arivon"
 include("arivon-common")
 include("arivon-sql-parent")
+include("arivon-sql-mysql")
+include("arivon-sql-mariadb")
+include("arivon-sql-h2")
+include("arivon-sql-postgresql")
+include("arivon-document-parent")
+include("arivon-document-redis")
