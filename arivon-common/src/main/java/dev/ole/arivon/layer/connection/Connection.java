0be690d75596e89ad870162ba7ade829c68e0c4e
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-25 16:21:35 +0200

    Add Connection interface to the connection layer
    This commit introduces a new Connection interface in the arivon-common module. The interface defines essential methods for handling connections, such as connect, isConnected, close, and query. This implementation will facilitate better abstraction and management of connection-related operations.

---
+++ arivon-common/src/main/java/dev/ole/arivon/layer/connection/Connection.java
@@ -0,0 +1,19 @@
+package dev.ole.arivon.layer.connection;
+
+import org.jetbrains.annotations.NotNull;
+
+public interface Connection<C, Q> {
+
+    void connect(ConnectionAuthentication credentials);
+
+    boolean isConnected();
+
+    void close();
+
+    C connection();
+
+    void update(Q query);
+
+    void query(@NotNull Q query);
+
+}
