c66ec9582be7f06cb08ee97b51a4bb22639f8207
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-25 16:41:04 +0200

    Add HikariLayerQuery implementation
    Introduced HikariLayerQuery class to support CRUD operations and specific aggregate functions. Integrated HikariProcess for handling different query processes such as create, update, delete, search, and mathematical operations.

---
+++ arivon-sql-parent/src/main/java/dev/ole/arivon/sql/parent/query/HikariLayerQuery.java
@@ -0,0 +1,101 @@
+package dev.ole.arivon.sql.parent.query;
+
+import dev.ole.arivon.Ordering;
+import dev.ole.arivon.Repository;
+import dev.ole.arivon.layer.Layer;
+import dev.ole.arivon.process.ProcessRunner;
+import dev.ole.arivon.query.Query;
+import dev.ole.arivon.query.QueryConstant;
+import dev.ole.arivon.query.QueryMethod;
+import dev.ole.arivon.sql.parent.process.*;
+import dev.ole.arivon.sql.parent.reference.HikariProcessReference;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+import org.jetbrains.annotations.Nullable;
+import java.math.BigDecimal;
+import java.util.List;
+
+@Accessors(fluent = true)
+@AllArgsConstructor
+public final class HikariLayerQuery<V> implements QueryMethod<V> {
+
+    private final Layer<?> layer;
+    @Getter
+    private final Repository<?> associatedRepository;
+    private ProcessRunner<HikariProcessReference> runner;
+
+    @Override
+    public void create(Query<?> query, V value) {
+        runner.apply(layer, query, new HikariCreateProcess(value));
+    }
+
+    @Override
+    public void update(Query<?> query, V value) {
+        runner.apply(layer, query, new HikariUpdateProcess(value));
+    }
+
+    @Override
+    public void delete(Query<?> query) {
+        runner.apply(layer, query, new HikariDeleteProcess());
+    }
+
+    @Override
+    public boolean exists(Query<?> query) {
+        return runner.apply(layer, query, new HikariCheckProcess());
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public @Nullable V findFirst(Query<?> query) {
+        query.limit(1);
+        var values = runner.apply(layer, query, new HikariSearchProcess());
+        if (!values.isEmpty()) {
+            return (V) values.get(0);
+        }
+        return null;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<V> find(Query<?> query) {
+        return (List<V>) runner.apply(layer, query, new HikariSearchProcess());
+    }
+
+    @Override
+    public long sum(Query<?> query, String id) {
+        return ((BigDecimal) runner.apply(layer, query, new HikariMathProcess<>("SUM(" + id + ")", BigDecimal.ZERO))).longValue();
+    }
+
+    @Override
+    public double average(Query<?> query, String id) {
+        //TODO has to be tested: maybe also use BigDecimal or BigDouble -> see sum()
+        return ((Double) runner.apply(layer, query, new HikariMathProcess<>("AVG(" + id + ")", 0D)));
+    }
+
+    @Override
+    public V min(String id) {
+        // todo
+        return null;
+    }
+
+    @Override
+    public V max(String id) {
+        //todo
+        return null;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<V> order(Query<?> query, String id, Ordering ordering) {
+        var process = new HikariSearchProcess();
+        process.constants().put(QueryConstant.ORDERING, id);
+        process.constants().put(QueryConstant.ORDERING_TYPE, ordering);
+        return (List<V>) runner.apply(layer, query, process);
+    }
+
+    @Override
+    public long count(Query<?> query) {
+        return runner.apply(layer, query, new HikariCountProcess());
+    }
+}
