6f79df48bea7c1b95ee1797770879fecd03848bf
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-25 16:22:10 +0200

    Add HikariFilterHandler, HikariFilter, and HikariFilterUtil classes
    Introduced new classes to handle and apply Hikari filtering logic. HikariFilterHandler implements various filtering methods, HikariFilter provides filter sequence handling, and HikariFilterUtil appends filters to SQL queries.

---
+++ arivon-sql-parent/src/main/java/dev/ole/arivon/sql/parent/HikariFilter.java
@@ -0,0 +1,38 @@
+package dev.ole.arivon.sql.parent;
+
+import dev.ole.arivon.filtering.Filter;
+import org.jetbrains.annotations.NotNull;
+
+public abstract class HikariFilter<R> extends Filter<String, R> {
+
+    public  HikariFilter(String id, R value) {
+        super(id, value);
+    }
+
+    public static final class SequenceMatchFilter extends HikariFilter<Object> {
+
+        private final String filterSequence;
+
+        public SequenceMatchFilter(String id, Object value, String filterSequence) {
+            super(id, value);
+            this.filterSequence = filterSequence;
+        }
+
+        @Override
+        public @NotNull String filter() {
+            return id() + " " + filterSequence + " ?";
+        }
+    }
+
+    static class Like extends HikariFilter<Object> {
+
+        public Like(String id, Object value) {
+            super(id, value);
+        }
+
+        @Override
+        public String filter() {
+            return id() + " LIKE ?";
+        }
+    }
+}
---
+++ arivon-sql-parent/src/main/java/dev/ole/arivon/sql/parent/HikariFilterHandler.java
@@ -0,0 +1,47 @@
+package dev.ole.arivon.sql.parent;
+
+import dev.ole.arivon.filtering.Filter;
+import dev.ole.arivon.filtering.FilterHandler;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+public final class HikariFilterHandler implements FilterHandler<String, Object> {
+
+    @Contract("_, _ -> new")
+    @Override
+    public @NotNull Filter<String, Object> match(String id, Object value) {
+        return new HikariFilter.SequenceMatchFilter(id, value, "=");
+    }
+
+    @Contract("_, _ -> new")
+    @Override
+    public @NotNull Filter<String, Object> noneMatch(String id, Object value) {
+        return new HikariFilter.SequenceMatchFilter(id, value, "!=");
+    }
+
+    @Contract("_, _ -> new")
+    @Override
+    public @NotNull Filter<String, Object> like(String id, String value) {
+        return new HikariFilter.Like(id, value);
+    }
+
+    @Override
+    public Filter<String, Object> matchIgnoreCase(String id, String value) {
+        return this.match(id, value);
+    }
+
+    @Override
+    public Filter<String, Object> between(String id, Number min, Number max) {
+        return null;
+    }
+
+    @Override
+    public Filter<String, Object> min(String id, Number min) {
+        return new HikariFilter.SequenceMatchFilter(id, min, ">=");
+    }
+
+    @Override
+    public Filter<String, Object> max(String id, Number max) {
+        return new HikariFilter.SequenceMatchFilter(id, max, "<=");
+    }
+}
