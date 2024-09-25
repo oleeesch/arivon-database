39ac2ba119a21f11a7f95f7c969c078922256c24
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-25 16:23:02 +0200

    Add QueryConstant class for query-related constants
    Introduced QueryConstant class to define and manage constants used in queries such as LIMIT, OFFSET, ORDERING, and ORDERING_TYPE. This class also includes a nested PrimaryShortCut class to manage primary key operations.

---
+++ arivon-common/src/main/java/dev/ole/arivon/query/QueryConstant.java
@@ -0,0 +1,64 @@
+package dev.ole.arivon.query;
+
+import dev.ole.arivon.Ordering;
+import dev.ole.arivon.RepositoryConstant;
+import dev.ole.arivon.RepositoryEntry;
+import dev.ole.arivon.common.Reflections;
+import dev.ole.arivon.constant.Constant;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public final class QueryConstant<T> extends Constant<T> {
+
+    public QueryConstant(String id) {
+        super(id);
+    }
+
+    public static final QueryConstant<Integer> LIMIT = constant("LIMIT");
+
+    public static final QueryConstant<Integer> OFFSET = constant("OFFSET");
+
+    public static final QueryConstant<String> ORDERING = constant("ORDERING");
+
+    public static final QueryConstant<Ordering> ORDERING_TYPE = constant("ORDERING_TYPE");
+
+    public static final QueryConstant<Void> RANDOMIZE = constant("RANDOMIZE");
+
+    public static final QueryConstant<PrimaryShortCut> PRIMARY_SHORTCUT = constant("SHORT_CUT");
+
+    @Contract("_ -> new")
+    private static <T> @NotNull QueryConstant<T> constant(String id) {
+        return new QueryConstant<>(id);
+    }
+
+    public static final class PrimaryShortCut {
+
+        private final Map<String, Object> primaries = new LinkedHashMap<>();
+
+        public static PrimaryShortCut append(@NotNull List<RepositoryEntry> primaries, Object parent) {
+            var shortCut = new PrimaryShortCut();
+
+            for (var primary : primaries) {
+                var primaryValue = Reflections.value(primary.constants().constant(RepositoryConstant.PARAM_FIELD), parent);
+                shortCut.primaries.put(primary.id(), primaryValue);
+            }
+            return shortCut;
+        }
+
+        public void add(RepositoryEntry entry, Object value) {
+            this.primaries.put(entry.id(), value);
+        }
+
+        public Object value(String id) {
+            return primaries.get(id);
+        }
+
+        public Object value(RepositoryEntry entry) {
+            return primaries.get(entry.id());
+        }
+    }
+}
