66a4030f167b3e781b8518d43bbb5d804944d80e
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-25 15:51:56 +0200

    Add RepositoryConstant class to manage constants
    Introduced the RepositoryConstant class to encapsulate common constants used across repositories. This includes definitions for field parameters, foreign references, primary keys, transformers, value rendering, and value refactoring. This change enhances code organization and reusability of constants.

---
+++ arivon-common/src/main/java/dev/ole/arivon/RepositoryConstant.java
@@ -0,0 +1,35 @@
+package dev.ole.arivon;
+
+import dev.ole.arivon.constant.Constant;
+import dev.ole.arivon.transformers.Transformer;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.function.Function;
+
+public final class RepositoryConstant<T> extends Constant<T> {
+
+    public RepositoryConstant(String id) {
+        super(id);
+    }
+
+    // repository constants for own field if is present
+    public static final RepositoryConstant<Field> PARAM_FIELD = constant("PARAM_FIELD");
+
+    public static final RepositoryConstant<List<RepositoryEntry>> FOREIGN_REFERENCE = constant("FOREIGN_REFERENCE");
+
+    public static final RepositoryConstant<Void> PRIMARY_KEY = constant("PRIMARY_KEY");
+
+    public static final RepositoryConstant<Transformer> TRANSFORMER = constant("TRANSFORMER");
+
+    public static final RepositoryConstant<Function<Object, Object>> VALUE_RENDERING = constant("VALUE_RENDERING");
+
+    public static final RepositoryConstant<Function<Object, Object>> VALUE_REFACTOR = constant("VALUE_WRITING");
+
+    @Contract("_ -> new")
+    private static <T> @NotNull RepositoryConstant<T> constant(String id) {
+        return new RepositoryConstant<>(id);
+    }
+}
