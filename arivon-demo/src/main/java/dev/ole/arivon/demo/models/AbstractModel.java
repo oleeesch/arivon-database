ca64c31cd4062327e2f1feb8816fbd48c0bc43b2
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-25 17:50:08 +0200

    Add AbstractModel class extending TestObject1
    Introduced a new AbstractModel class that extends TestObject1. This class includes a UUID field named 'test' and initializes it via the constructor. Annotations for getters, toString, and fluent accessors are also added.

---
+++ arivon-demo/src/main/java/dev/ole/arivon/demo/models/AbstractModel.java
@@ -0,0 +1,21 @@
+package dev.ole.arivon.demo.models;
+
+import dev.ole.arivon.demo.models.objects.TestObject1;
+import lombok.Getter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+import java.util.UUID;
+
+@Getter
+@Accessors(fluent = true)
+@ToString
+public final class AbstractModel extends TestObject1 {
+
+    private final UUID test;
+
+    public AbstractModel(String permission, int expire, UUID test) {
+        super(permission, expire);
+        this.test = test;
+    }
+}
