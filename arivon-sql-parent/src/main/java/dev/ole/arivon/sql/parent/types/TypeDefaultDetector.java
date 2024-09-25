861cf7178ea3532c21c79983e4134a7ed45818c0
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-25 16:42:34 +0200

    Add TypeDefaultDetector for type detection and handling
    Implemented `TypeDefaultDetector` to manage type overwriting and type detection in `TypeDetector`. Supports common types and enums, ensuring proper handling and value rendering for enums.

---
+++ arivon-sql-parent/src/main/java/dev/ole/arivon/sql/parent/types/TypeDefaultDetector.java
@@ -0,0 +1,55 @@
+package dev.ole.arivon.sql.parent.types;
+
+import dev.ole.arivon.RepositoryConstant;
+import dev.ole.arivon.RepositoryEntry;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.UUID;
+
+public final class TypeDefaultDetector implements TypeDetector {
+
+    private final List<Type> detectCollection = new LinkedList<>();
+
+    @SuppressWarnings("unchecked")
+    public TypeDefaultDetector() {
+        this.overwrite(Type.of("TINYINT", byte.class, Byte.class));
+        this.overwrite(Type.of("INT", int.class, Integer.class));
+        this.overwrite(Type.of("BIGINT", long.class, Long.class));
+        this.overwrite(Type.of("FLOAT", float.class, Float.class));
+        this.overwrite(Type.of("DOUBLE", double.class, Double.class));
+        this.overwrite(Type.of("BOOL", boolean.class, Boolean.class));
+        this.overwrite(Type.of("CHAR", char.class, Character.class));
+        this.overwrite(Type.of("UUID", UUID.class));
+        this.overwrite(Type.of("TEXT", String.class));
+
+        // enums has a collection of all elements as string behind the name, we must duplicate every enum type
+        this.overwrite(TypeModel.of(it -> "ENUM('" + String.join("', '", Arrays.stream(((Class<? extends Enum<?>>) it).getEnumConstants()).map(Enum::name).toList()) + "')", entry -> entry.clazz().isEnum()));
+    }
+
+    @Override
+    public void overwrite(Type type) {
+        detectCollection.add(0, type);
+    }
+
+    @Override
+    public void removeType(String typeId) {
+        this.detectCollection.removeAll(this.detectCollection.stream().filter(it -> it.toString().equals(typeId)).toList());
+    }
+
+    @Contract(pure = true)
+    @Override
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    public @NotNull Type detect(@NotNull RepositoryEntry entry) {
+        var type = this.detectCollection.stream().filter(it -> it.predicate().test(entry)).findFirst().map(t -> t instanceof TypeModel typeModel ? typeModel.model(entry.clazz()) : t).orElseThrow(() -> new UnsupportedOperationException("For type: " + entry.clazz().getSimpleName()));
+
+        // we must check if enum type is present - add value rendering
+        if(entry.clazz().isEnum()) {
+            entry.constants().put(RepositoryConstant.VALUE_RENDERING, o -> Enum.valueOf(((Class<? extends Enum>)entry.clazz()), o.toString()));
+        }
+        return type;
+    }
+}
