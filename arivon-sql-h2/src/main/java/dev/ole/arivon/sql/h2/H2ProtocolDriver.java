0a9c5452d937b8651024475814719395b062089a
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-25 17:36:54 +0200

    Add H2ProtocolDriver class implementation
    Introduced the H2ProtocolDriver class for H2 database connection handling. This class initializes the H2 driver and constructs JDBC URLs using provided authentication credentials. It ensures automatic server mode and reconnect functionality.

---
+++ arivon-sql-h2/src/main/java/dev/ole/arivon/sql/h2/H2ProtocolDriver.java
@@ -0,0 +1,20 @@
+package dev.ole.arivon.sql.h2;
+
+import dev.ole.arivon.sql.parent.driver.ProtocolDriverLoader;
+import org.h2.Driver;
+import org.jetbrains.annotations.NotNull;
+
+import java.nio.file.Path;
+
+public final class H2ProtocolDriver implements ProtocolDriverLoader<H2ConnectionAuthentication> {
+
+    @Override
+    public void initialize() {
+        Driver.load();
+    }
+
+    @Override
+    public @NotNull String jdbcUrl(@NotNull H2ConnectionAuthentication credentials) {
+        return "jdbc:h2:" + Path.of(credentials.path()).toAbsolutePath() + ";AUTO_SERVER=TRUE;AUTO_RECONNECT=TRUE";
+    }
+}
