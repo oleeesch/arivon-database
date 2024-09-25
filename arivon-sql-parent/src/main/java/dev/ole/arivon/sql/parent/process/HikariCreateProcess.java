a59ee728e9a3c4cad21a390ecedc341052a58b08
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-25 16:40:53 +0200

    Add HikariCreateProcess for SQL insert operations
    Introduce a new class, `HikariCreateProcess`, to handle SQL insert commands efficiently. This class extends `UpdateProcess` and incorporates logic to handle different entry types, including collection and map entries, ensuring comprehensive data insertion.

---
+++ arivon-sql-parent/src/main/java/dev/ole/arivon/sql/parent/process/HikariCreateProcess.java
@@ -0,0 +1,81 @@
+package dev.ole.arivon.sql.parent.process;
+
+import dev.ole.arivon.RepositoryConstant;
+import dev.ole.arivon.RepositoryExternalEntry;
+import dev.ole.arivon.common.Pair;
+import dev.ole.arivon.common.Reflections;
+import dev.ole.arivon.external.RepositoryCollectionEntry;
+import dev.ole.arivon.external.RepositoryMapEntry;
+import dev.ole.arivon.process.kind.UpdateProcess;
+import dev.ole.arivon.query.QueryConstant;
+import dev.ole.arivon.sql.parent.HikariFilter;
+import dev.ole.arivon.sql.parent.reference.HikariProcessReference;
+import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+@AllArgsConstructor
+public final class HikariCreateProcess extends UpdateProcess<HikariProcessReference, HikariFilter<Object>> {
+
+    private static final String CREATE_VALUE_SQL = "INSERT INTO %s (%s) VALUES (%s);";
+    private final Object value;
+
+    @Override
+    @SneakyThrows
+    public void run(@NotNull RepositoryExternalEntry entry, HikariProcessReference reference) {
+        var elements = new ArrayList<String>();
+        var arguments = new ArrayList<>();
+
+        // Since lists can also have individual attributes as types, we have to check whether these are present,
+        // otherwise serialization can be carried out as normal.
+        if (entry instanceof RepositoryCollectionEntry collectionEntry && !(collectionEntry.typeEntry() instanceof RepositoryExternalEntry)) {
+            elements.add(collectionEntry.typeEntry().id());
+            arguments.add(value);
+            // maps are specific parameters there have not 1 value, rather more.
+        } else if (entry instanceof RepositoryMapEntry mapEntry && !(mapEntry.keyEntry() instanceof RepositoryExternalEntry && mapEntry.valueEntry() instanceof RepositoryExternalEntry)) {
+            var keyEntry = mapEntry.keyEntry();
+            var valueEntry = mapEntry.valueEntry();
+
+            elements.add(keyEntry.id());
+            elements.add(valueEntry.id());
+
+            var data = (Pair<?, ?>) value;
+
+            arguments.add(keyEntry.constants().has(RepositoryConstant.VALUE_REFACTOR) ? keyEntry.constants().constant(RepositoryConstant.VALUE_REFACTOR).apply(data.first()) : data.first());
+            arguments.add(valueEntry.constants().has(RepositoryConstant.VALUE_REFACTOR) ? valueEntry.constants().constant(RepositoryConstant.VALUE_REFACTOR).apply(data.second()) : data.second());
+        } else {
+            for (var child : entry.children()) {
+                var childValue = Reflections.value(child.constants().constant(RepositoryConstant.PARAM_FIELD), value);
+                if (child instanceof RepositoryExternalEntry externalEntry) {
+                    for (var object : externalEntry.readValues(childValue)) {
+                        var subprocess = new HikariCreateProcess(object);
+
+                        // we put all parent primaries in the next process
+                        subprocess.constants().put(QueryConstant.PRIMARY_SHORTCUT, QueryConstant.PrimaryShortCut.append(entry.primaries(), value));
+
+                        // append the sub process
+                        subprocess.run(externalEntry, reference);
+                    }
+                } else {
+                    elements.add(child.id());
+
+                    if (child.constants().has(RepositoryConstant.VALUE_REFACTOR)) {
+                        arguments.add(child.constants().constant(RepositoryConstant.VALUE_REFACTOR).apply(childValue));
+                    } else {
+                        arguments.add(childValue);
+                    }
+                }
+            }
+        }
+
+        if (entry.constants().has(RepositoryConstant.FOREIGN_REFERENCE)) {
+            for (var foreignKey : entry.constants().constant(RepositoryConstant.FOREIGN_REFERENCE)) {
+                arguments.add(0, constants().constant(QueryConstant.PRIMARY_SHORTCUT).value(foreignKey));
+                elements.add(0, foreignKey.id());
+            }
+        }
+        reference.append(CREATE_VALUE_SQL.formatted(entry.id(), String.join(", ", elements), String.join(", ", "?".repeat(elements.size()).split(""))), arguments.toArray());
+    }
+}
