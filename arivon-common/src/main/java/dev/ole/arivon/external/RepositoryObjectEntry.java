39f46d91c0d912ec3871d76f94bdeb30b86bb65f
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-25 15:52:26 +0200

    Add RepositoryObjectEntry class for handling entity fields
    This commit introduces the `RepositoryObjectEntry` class, which extends `RepositoryExternalEntry`. It manages entity fields by reading them through the `ClassFieldReader` and applying specific field-level conditions, such as handling annotations like `Row` and `PrimaryKey`. This enhancement will streamline object field processing in the repository layer.

---
+++ arivon-common/src/main/java/dev/ole/arivon/external/RepositoryObjectEntry.java
@@ -0,0 +1,62 @@
+package dev.ole.arivon.external;
+
+import dev.ole.arivon.*;
+import dev.ole.arivon.common.ClassFieldReader;
+import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+@Getter
+public final class RepositoryObjectEntry extends RepositoryExternalEntry {
+
+    public RepositoryObjectEntry(String id, Class<?> clazz, RepositoryExternalEntry parent) {
+        super(id, clazz, parent);
+
+        if (!clazz.isAnnotationPresent(Entity.class) || !clazz.getDeclaredAnnotation(Entity.class).ignoreSuperClass()) {
+            for (var field : ClassFieldReader.allFields(clazz)) {
+                applyField(field);
+            }
+        } else {
+            for (var field : ClassFieldReader.fields(clazz)) {
+                applyField(field);
+            }
+        }
+
+        for (var child : children()) {
+            if (!(child instanceof RepositoryExternalEntry)) {
+                continue;
+            }
+            child.constants().put(RepositoryConstant.FOREIGN_REFERENCE, primaries());
+        }
+    }
+
+    private void applyField(@NotNull Field field) {
+        // arivon does not allow static fields, only object parameters
+        if (Modifier.isStatic(field.getModifiers())) {
+            return;
+        }
+
+        var fieldId = field.getName();
+
+        if (field.isAnnotationPresent(Row.class)) {
+            var row = field.getDeclaredAnnotation(Row.class);
+
+            if (row.ignore()) {
+                return;
+            }
+
+            if (!row.id().isEmpty()) {
+                fieldId = row.id();
+            }
+        }
+
+        var entry = RepositoryEntryFinder.find(field, fieldId, this);
+        entry.constants().put(RepositoryConstant.PARAM_FIELD, field);
+
+        if (field.isAnnotationPresent(PrimaryKey.class)) {
+            entry.constants().option(RepositoryConstant.PRIMARY_KEY);
+        }
+        children(entry);
+    }
+}
